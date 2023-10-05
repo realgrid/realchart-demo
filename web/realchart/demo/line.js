6fa6957250cc7a6d38a0b2e9b1c1cf1a12c01d5e
WooriPbg <wooripbg@gmail.com>
2023-10-05 15:31:57 +0900

    line chart big data

--- web/realchart/demo/line.js
+++ web/realchart/demo/line.js
@@ -6,23 +6,30 @@ const config = {
     options: {},
     title: "Line Series 01",
     xAxis: {
-        type: 'category'
+        type: 'category',
+        label: {
+            step: 300
+        }
     },
     yAxis: {
+        minValue: 0,
+        maxValue: 100,
     },
     series: {
         type: 'line',
-        pointLabel: true,
-        data: [
-            ['home', 7], 
-            ['sky', 11], 
-            ['카눈', 8], 
-            ['def', 9], 
-            ['머핀', 11], 
-            ['지리산', 15.3], 
-            ['zzz', 13],
-            ['낙동강', 12.5]
-        ]
+        marker: false,
+        // pointLabel: true,
+        // data: [
+        //     ['home', 7], 
+        //     ['sky', 11], 
+        //     ['카눈', 8], 
+        //     ['def', 9], 
+        //     ['머핀', 11], 
+        //     ['지리산', 15.3], 
+        //     ['zzz', 13],
+        //     ['낙동강', 12.5]
+        // ]
+        data: line_data
     }
 }
 
