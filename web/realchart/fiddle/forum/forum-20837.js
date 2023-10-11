017d7be669d5a5c640213ac62fbc7f2a3700334a
dltlghkd930217 <58334987+dltlghkd930217@users.noreply.github.com>
2023-10-11 09:31:11 +0900

    forum 답변 fiddle 추가

---
+++ web/realchart/fiddle/forum/forum-20837.js
@@ -0,0 +1,45 @@
+
+const config = {
+	title: '2022년도 도시의 평균 대기질 지수',
+	options: {
+		// animatable: false
+	},
+	xAxis: {
+		title: '수정구',
+		categories: ['2014', '2015', '2016', '2017', '2018', '2019', '2020'],
+		grid: true,
+		label:{
+			step: 3
+		}
+	},
+	yAxis: {
+		title: '대기질 지수(Air Quality Index, AQI)',
+	},
+	series: {
+		name: '대기질',
+		pointLabel: {
+			visible: true,
+			numberSymbols: ""
+		},
+		data: [
+			['신흥1동', 12901204],
+			['신흥2동', 197960],
+			['신흥3동', 109959],
+			['태평1동', 146250],
+			['태평2동', 146270],
+			['태평3동', 126409],
+			['태평4동', 122079],
+		],
+	},
+	ChartTextEffect: {
+		autoContrast: false,
+	},
+};
+
+let animate = false;
+let chart;
+
+
+function init() {
+	chart = RealChart.createChart(document, 'realchart', config);
+}
