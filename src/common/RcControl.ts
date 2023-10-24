04892c28726a6ac7afd15b979414c9b19ba50020
dltlghkd930217 <58334987+dltlghkd930217@users.noreply.github.com>
2023-10-24 15:40:02 +0900

    package.json 버전 수정 0.9.9

--- src/common/RcControl.ts
+++ src/common/RcControl.ts
@@ -393,7 +393,7 @@ export abstract class RcControl extends RcWrappableObject {
 
         const desc = doc.createElement('desc');
         // desc.textContent = 'Created by RealChart v$Version'; // sourcemap, rollup issue
-        desc.textContent = 'Created by RealChart v0.9.8';
+        desc.textContent = 'Created by RealChart v0.9.9';
         svg.appendChild(desc);
 
         const defs = this._defs = doc.createElementNS(SVGNS, 'defs');
