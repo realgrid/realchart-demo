c1a3db8e8446630e8d4549d295801698b48f6b48
WooriPbg <wooripbg@gmail.com>
2023-11-06 10:45:33 +0900

    docs demo순서 web과 동일하게 변경

--- docs/public/realchart/realchart-lic.js
+++ docs/public/realchart/realchart-lic.js
@@ -1,2 +1,2 @@
-// 아래 라이선스는 유효기간 2024년 2월 2일 까지 localhost 도메인에서 사용할 수 있는 라이선스 입니다.
+// 아래 라이선스는 유효기간 2024년 2월 2일 까지 localhost, *.realhart.co.kr 도메인에서 사용할 수 있는 라이선스 입니다.
 var realChartLic = 'upVcPE+wPOkOR/egW8JuxkM/nBOseBrflwxYpzGZyYm9cY8amGDkiNBG+1qQWvM8rPfx2mswOIepkUNPzph8+V7svsLh1nO8rZZqhg+Lls0pIgP5fDmLqNBf/fferiyN';
--- docsDemo.js
+++ docsDemo.js
@@ -1,5 +1,5 @@
 import { chromium } from '@playwright/test';
-import { existsSync, mkdirSync, readFileSync, rmdirSync, copyFileSync } from 'fs';
+import { existsSync, mkdirSync, readFileSync, rmdirSync, copyFileSync, writeFileSync } from 'fs';
 import fs from 'fs/promises';
 import util from 'util';
 
@@ -7,6 +7,22 @@ function readFile(file) {
     return readFileSync(file, "utf-8");
 }
 
+function createMetaJson() {
+    const list = JSON.parse(readFile('./web/realchart/demos.json'));
+	let categories = {};
+	const map = new Map(Object.entries(list));
+	map.forEach((value, key) => {
+		categories[key.replace(' ', '')] = {title: key};
+		const map2 = new Map(Object.entries(value));
+		let demos = {}
+		map2.forEach((value2, key2) => {
+			// console.log(`${key}: ${value}`);
+			demos[value2] = {title: key2};
+		});
+		writeFileSync(`./docs/pages/demo/${key.replace(' ', '')}/_meta.json`, JSON.stringify(demos, null, '\t'));
+	});
+	writeFileSync(`./docs/pages/demo/_meta.json`, JSON.stringify(categories, null, '\t'));
+}
 
 (async () => {
     if (existsSync('./docs/public/realchart')) {
@@ -30,13 +46,13 @@ function readFile(file) {
 
     const baseUrl = 'http://localhost:6010/realchart/';
 	const targetUrl = './docs/pages/';
-    const categori = 'demo/';
+    const category = 'demo/';
 
     const info = JSON.parse(readFile('./web/realchart/demos.json'));
 
     let count = 0;
     for (const [key, value] of Object.entries(info)) {
-		const target = targetUrl + categori + key.replace(/ /g, '');
+		const target = targetUrl + category + key.replace(/ /g, '');
         if (!existsSync(target)) {
             mkdirSync(target, { recursive: true })
             console.log(`${target} 해당 경로에 폴더가 없어 생성되었습니다.`)
@@ -44,14 +60,14 @@ function readFile(file) {
 
         if (typeof value !== 'object') continue;
         for (const [key2, value2] of Object.entries(value)) {
-            await page.goto(baseUrl + categori + value2 + '.html');
+            await page.goto(baseUrl + category + value2 + '.html');
             const config = await page.evaluate('config');
-			await fs.writeFile('./docs/templates/' + value2 + '.js', `export const config = ${util.inspect(config, { depth: null, maxArrayLength: null })}
+			writeFileSync('./docs/templates/' + value2 + '.js', `export const config = ${util.inspect(config, { depth: null, maxArrayLength: null })}
 `);
             ++count;
             console.log(count, `./docs/templates/${value2}.js`);
             
-			await fs.writeFile(target + '/' + value2 + '.mdx', `---
+			writeFileSync(target + '/' + value2 + '.mdx', `---
 title: "${key2}"
 ---
 import { RealChartReact } from "@/components/RealChart/RealChartReact";
@@ -66,6 +82,13 @@ import { config } from "@/templates/${value2}";
         };
     };
 
-    console.log(`총 ${count}개의 파일을 생성했습니다.`)
+    console.log(`총 ${count}개의 mdx파일을 생성했습니다.`);
+	try {
+		createMetaJson();
+		console.log('_meta.json 파일 생성에 성공했습니다.')
+	} catch(e) {
+		console.error(`_meta.json 파일 생성에 실패했습니다.
+		${e}`)
+	}
     process.exit();
 })();
