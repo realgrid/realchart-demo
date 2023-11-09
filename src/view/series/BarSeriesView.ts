3c642073b4596a8fe645a7750babd95c2390e763
WooriPbg <wooripbg@gmail.com>
2023-11-09 12:44:42 +0900

    polar

--- src/view/series/BarSeriesView.ts
+++ src/view/series/BarSeriesView.ts
@@ -126,14 +126,14 @@ export class BarSeriesView extends BoxedSeriesView<BarSeries> {
             const p = view.point;
             const y = yAxis.getPosition(polar.rd, p.yGroup) * vr;
             const wUnit = xAxis.getUnitLength(Math.PI * 2, p.xValue);
-            const wPoint = series.getPointWidth(wUnit) * 2;
+            const wPoint = series.getPointWidth(wUnit);
     
             view.setSector({
                 cx: polar.cx, 
                 cy: polar.cy, 
                 rx: y, 
                 ry: y,
-                start: polar.start + (i + 0.5) * polar.deg,
+                start: polar.start + (i + 0.25) * polar.deg,
                 angle: wPoint,
                 clockwise: true
             })
