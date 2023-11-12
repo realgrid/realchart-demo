a1ba98b9197abe6c6d2874be89bde115919d3573
benny-yoo <123144980+benny-yoo@users.noreply.github.com>
2023-11-12 18:31:32 +0900

    Update dldoc

--- dldoc.js
+++ dldoc.js
@@ -1,18 +1,37 @@
+////////////////////////////////////////////////////////////////////////////////
+// dldoc.js
+// 2023. 09. 17. created by benny
+// -----------------------------------------------------------------------------
+// Copyright (c) 2023 Wooritech Inc.
+// All rights reserved.
+////////////////////////////////////////////////////////////////////////////////
+
 /**
- * typedoc으로 가공한 json을 가지고 재가공한다.
- * 
+ * typedoc으로 가공한 ./api/model.json을 가지고 재가공한다. 따라서 typedoc이 먼저 실행되어야 한다.
+ * ```yarn dldoc``` script에 해당 명령을 포함한다.
  * export 한 클래스만 찾는다. export하지 않은 것을 포함하려면 typedoc plugin 필요.
  * https://github.com/tomchen/typedoc-plugin-not-exported
  */
 import fs from 'fs';
-import jsdom from 'jsdom';
-const { JSDOM } = jsdom;
+import path from 'path';
 
-const text = fs.readFileSync('./api/model.json', { encoding: 'utf-8'});
-const model = JSON.parse(text);
-const classMap = {};
-
+const JSFIDDLE_URL = 'https://jsfiddle.net/gh/get/library/pure/realgrid/realchart-demo/tree/master/';
+/**
+ * typedoc에서 가공한 클래스 구조 모델을 api문서에서 사용할 config 구조로 재가공한다.
+ */
 class Tunner {
+  constructor({ path = '', debug = false }) {
+    this.debug = debug;
+    const text = fs.readFileSync(path || './docs/.tdout/model.json', { encoding: 'utf-8'});
+    this.model = JSON.parse(text);
+    this.classMap = {};
+  }
+
+  static get fiddleUrl() {
+    return JSFIDDLE_URL;
+  }
+
+
   _findTag(tags, tag) {
     return tags?.find(t => t.tag == tag );
   }
@@ -195,7 +214,7 @@ class Tunner {
         // console.debug({ kindString, parameters, type: _type });
       default:
         // class, Interface 인 경우, type 없음.
-        name && console.warn(`ARN] Unexpected type ${name}:${obj.kindString}`);
+        name && console.warn(`[WARN] Unexpected type ${name}:${obj.kindString}`);
         return obj.kindString === 'Interface' ? name : '';
     }
   }
@@ -655,170 +674,6 @@ class MDGenerater {
   }
 }
 
-const parseSummary = (summary) => {
-  return summary?.map(line => {
-    switch (line.kind) {
-      case 'inline-tag':
-        return `{${line.tag} ${line.text}}`
-      case 'text':
-        return line.text
-    }
-  }).join(' ');
-}
-const parseComment = (comment) => {
-  const { summary, blockTags } = { ...comment };
-  const lines = parseSummary(summary);
-  // @config content
-  const config = parseBlockTags(blockTags)
-  return [config, lines];
-}
-const setContent = (prop) => {
-  // return prop.name;
-  const [header, content] = parseComment(prop.comment);
-  return {
-      name: prop.name,
-      type: prop.type.name,
-      header, 
-      content,
-      defaultValue: prop.defaultValue
-  };
-}
-// scan all classes
-const visit = (obj) => {
-  const { name, children, kindString, comment, extendedTypes = [], extendedBy = [] } = { ...obj };
-  const [header, content] = parseComment(comment)
-  switch (kindString) {
-    case 'Class':
-      const regex = /(\w+)/g;
-      let matches = header?.matchAll(regex);
-      matches = matches && [...matches].map(m => m[0]);
-      const category = header ? (matches[0] == 'chart' ? matches[1] : header) : '';
-      classMap[name] = { 
-        header, content, category,
-        extended: extendedTypes.map(t => t.name),
-        props: children.filter(c => 
-          c.kindString == 'Property'
-          && findTag(c.comment?.blockTags, '@config')
-        ).map(setContent)
-      };
-      break;
-  }
-
-  children?.forEach(child => {
-    visit(child);
-  });
-
-  return extendedTypes.map(type => type.name);
-}
-
-visit(model);
-
-// const derive = (className) => {
-//   const clazz = classMap[className];
-//   if (clazz) {
-//     const { extended, props } = clazz;
-//     // const ext = classMap[className].extended;
-//     const [lastExt] = extended.slice(-1);
-//     if (lastExt) {
-//       // extended.splice(extended.length, 0, 
-//       //   ...derive(lastExt).filter((e) => !extended.includes(e)));
-//       extended.push(...derive(lastExt).filter((e) => !extended.includes(e)));
-//       // const { props:_props } = _classMap[className];
-//       // props.push(...derive(l));
-//       return extended;
-
-//     }
-    
-//   }
-
-//   return [className];
-// }
-
-// const _derive = (className, agg) => {
-//   // const clazz = JSON.parse(JSON.stringify(classMap[className]));
-//   const clazz = classMap[className] ?? {};
-//   const { extended=[], props=[] } = clazz;
-//   const [lastExt] = extended.slice(-1);
-//   if (lastExt) {
-//     console.debug(lastExt, agg.props.length);
-//     return derive(lastExt, {
-//       ...agg,
-//       derived: [...agg.derived, lastExt],
-//       // props: [...agg.props, ...props]
-//       props: clazz.props,
-//     });
-//   }
-//   return agg;
-// }
-
-// const _classMap = JSON.parse(JSON.stringify(classMap));
-// const _classMap = {};
-// Object.entries(classMap).map(([key, value]) => {
-//   derive(key);
-//   // _classMap[key] = derive(key, { derived:[], props:[] });
-//   // console.debug(_classMap[key])
-//   console.debug('-------------------------')
-// })
-
-const json = JSON.stringify(classMap, null, 2)
-// console.log(json);
-// console.log(JSON.stringify(classMap, null, 2));
-fs.writeFileSync('./api/api.json', json, { encoding: 'utf-8'})
-
-
-const dom = new JSDOM(`<!DOCTYPE html>
-<html>
-<body>
-  <header><h2>RealChart</h2></header>
-  <nav></nav>
-  <section></section>
-  <aside></aside>
-  <footer></footer>
-</body>
-</html>
-`);
-const doc = dom.window.document;
-const body = doc.body;
-const nav = body.querySelector('nav');
-
-const rootList = doc.createElement('ul');
-rootList.innerHTML = '>';
-const seriesList = doc.createElement('ul');
-seriesList.innerHTML = 'series>';
-const axisList = doc.createElement('ul');
-axisList.innerHTML = 'axis>';
-
-nav.appendChild(rootList);
-nav.appendChild(seriesList);
-nav.appendChild(axisList);
-
-const makePropList = (props) => {
-  const ul = doc.createElement('ul');
-  props.forEach(p => {
-    let li = doc.createElement('li');
-    li.innerHTML = p.name
-    ul.appendChild(li);
-  });
-  return ul;
-}
-
-Object.entries(classMap).forEach(([key, value]) => {
-  const { category } = value;
-  let li = doc.createElement('li');
-  // %caution% innerText is not implemented in jsdom.
-  li.innerHTML = key;
-  li.appendChild(makePropList(value.props));
-  switch (category) {
-    case 'series':
-      seriesList.appendChild(li);
-      break;
-    case 'axis':
-      axisList.appendChild(li);
-      break;
-    default:
-      rootList.appendChild(li);
-      break;
-  }
-});
-
-fs.writeFileSync('./api/index.html', doc.documentElement.outerHTML, { encoding: 'utf-8'});
+const classMap = new Tunner({debug:true}).scan();
+const generator = new MDGenerater(classMap, { debug:true })
+generator.run();
