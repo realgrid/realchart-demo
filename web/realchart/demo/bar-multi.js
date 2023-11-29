cd12315c8ebec4f784e67c5657f9ce72a511632b
rasangchul <87007118+rasangchul@users.noreply.github.com>
2023-11-29 14:52:23 +0900

    bar -> bar-multi 로 교체

--- web/realchart/demo/bar-multi.js
+++ web/realchart/demo/bar-multi.js
@@ -1,38 +1,48 @@
 /**
  * @demo
  *
+ * Bar Series 기본 예제.
  */
 const config = {
-	title: '한국가스공사 월간 시도별 도시가스 판매현황',
-	options: {},
+	title: '세종특별자치시 1인당 지방세 부담액',
+	options: {
+		// animatable: false
+	},
 	xAxis: {
-		title: '시도별',
-		categories: ['강원', '서울', '경기', '인천', '부산', '경북'],
+		title: '년도',
+		categories: ['2017', '2018', '2019', '2020', '2021', '2022',],
 		grid: true,
+		
+		label: {
+			// startStep: 0,
+			step: 1,
+		},
 	},
 	yAxis: {
-		title: 'Vertical 수직축 Axis',
+		title: '인구수',
+		// reversed: true,
+		// baseValue: -1,
+        // strictMin: 11,
+        // strictMax: 161
 	},
-	series: [
-		{
-			name: '2019년도',
-			pointLabel: true,
-			data: [413340, 4295799, 4582903, 1504513, 1428640, 1495929],
-		},
-		{
-			name: '2020년도',
-			pointWidth: 2,
-			pointLabel: true,
-			data: [416570, 4180225, 5236434, 1393145, 1408886, 1479257],
-		},
-		{
-			name: '2021년도',
-			pointLabel: true,
-			data: [459931, 4201860, 5498483, 1472529, 1316482, 1421999],
-		},
-	],
+	series: [{
+		name: '주민1인당 부담금액',
+		// baseValue: null,
+		pointLabel: true,
+		// pointWidth: '100%',
+		// colorByPoint: true,
+		data: [237466, 213497, 195827, 223488, 235850, 224338]
+	}, {
+		name: '인구수',
+		// baseValue: null,
+		pointLabel: true,
+		// pointWidth: '100%',
+		// colorByPoint: true,
+		data: [28010, 31412, 34057, 35583, 37189, 38359]
+	}],
 };
 
+let animate = false;
 let chart;
 
 function setActions(container) {
@@ -45,15 +55,32 @@ function setActions(container) {
 		},
 		false
 	);
+	createCheckBox(
+		container,
+		'Always Animate',
+		function (e) {
+			animate = _getChecked(e);
+		},
+		false
+	);
 	createButton(container, 'Test', function (e) {
 		alert('hello');
 	});
 	createCheckBox(
 		container,
+		'ColorByPoint',
+		function (e) {
+			config.series.colorByPoint = _getChecked(e);
+			chart.load(config, animate);
+		},
+		false
+	);
+	createCheckBox(
+		container,
 		'Inverted',
 		function (e) {
-			config.inverted = _getChecked(e);
-			chart.load(config);
+            config.inverted = _getChecked(e);
+			chart.load(config, animate);
 		},
 		false
 	);
@@ -62,7 +89,7 @@ function setActions(container) {
 		'X Reversed',
 		function (e) {
 			config.xAxis.reversed = _getChecked(e);
-			chart.load(config);
+			chart.load(config, animate);
 		},
 		false
 	);
@@ -71,7 +98,7 @@ function setActions(container) {
 		'Y Reversed',
 		function (e) {
 			config.yAxis.reversed = _getChecked(e);
-			chart.load(config);
+			chart.load(config, animate);
 		},
 		false
 	);
--- web/realchart/demo/bar.js
+++ web/realchart/demo/bar.js
@@ -4,42 +4,46 @@
  * Bar Series 기본 예제.
  */
 const config = {
-	title: '세종특별자치시 1인당 지방세 부담액',
+	title: '년도별 서울시 평균 대기질 지수',
 	options: {
 		// animatable: false
 	},
 	xAxis: {
-		title: '년도',
-		categories: ['2017', '2018', '2019', '2020', '2021', '2022',],
+		title: '서울시',
+		categories: ['2014', '2015', '2016', '2017', '2018', '2019', '2020'],
 		grid: true,
 		
 		label: {
 			// startStep: 0,
-			step: 1,
+			step: 3,
 		},
 	},
 	yAxis: {
-		title: '인구수',
+		title: '대기질 지수(Air Quality Index, AQI)',
 		// reversed: true,
 		// baseValue: -1,
         // strictMin: 11,
         // strictMax: 161
 	},
 	series: [{
-		name: '주민1인당 부담금액',
+		name: '대기질',
 		// baseValue: null,
 		pointLabel: true,
 		// pointWidth: '100%',
 		// colorByPoint: true,
-		data: [237466, 213497, 195827, 223488, 235850, 224338]
-	}, {
-		name: '인구수',
+		data: [155, 138, 122, 133, 114, 113, 123]
+	},{
+		name: '대기질2',
 		// baseValue: null,
 		pointLabel: true,
 		// pointWidth: '100%',
 		// colorByPoint: true,
-		data: [28010, 31412, 34057, 35583, 37189, 38359]
-	}],
+		data: [55, 38, 22, 33, 14, 13, 23]
+	},
+],
+	ChartTextEffect: {
+		autoContrast: false,
+	},
 };
 
 let animate = false;
