7c466652bec94ca80665f2bbcd931a05cc3e1f1d
WooriPbg <wooripbg@gmail.com>
2023-12-01 10:52:15 +0900

    snapshot test 병렬화 (#235)

--- test/e2e/snapshot-test/snapshotTest.spec.js
+++ test/e2e/snapshot-test/snapshotTest.spec.js
@@ -6,25 +6,26 @@ function readFile(file) {
 }
 
 const snapshotTestPage = 'http://localhost:6010/snapshot.html?debug';
-
 const ingnoreList = ['gauge-clock.js', 'gauge-clock-multi.js'];
 
-test('snapshot-test', async () => {
-    const browser = await chromium.launch();
-    const page = await browser.newPage();
+const root = './docs/templates/';
+const demos = readdirSync(root);
 
-    const root = './docs/templates/';
-    const demos = readdirSync(root);
+for (let i = 0; i < demos.length; i++) {
+    if (ingnoreList.includes(demos[i])) continue;
 
-    await page.goto(snapshotTestPage);
-
-    for (let i = 0; i < demos.length; i++) {
-        if (ingnoreList.includes(demos[i])) continue;
-        const config = readFile(root + demos[i]).replace('export const config = ', '');
+    test(demos[i].replace('.js', '') + '-test', async () => {
+        const browser = await chromium.launch();
+        const page = await browser.newPage();
+        await page.goto(snapshotTestPage);
         
+        const config = readFile(root + demos[i]).replace('export const config = ', '');
         await page.evaluate(`chart.load(${config}, false)`);
 
         const snapshot = await page.locator('#realchart').screenshot();
         await expect(snapshot).toMatchSnapshot(demos[i].replace('.js', '.png'));
-    }
-});
+
+        await browser.close();
+    })
+    
+};
