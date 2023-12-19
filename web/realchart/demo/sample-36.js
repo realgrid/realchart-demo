a2090227916686665cbe794e13c10cf940717190
dltlghkd930217 <58334987+dltlghkd930217@users.noreply.github.com>
2023-12-19 15:38:38 +0900

    update sample-36

--- web/realchart/demo/sample-36.js
+++ web/realchart/demo/sample-36.js
@@ -2,57 +2,99 @@
  * @demo
  */
 let config = {
-  type: 'line',
-  options: {},
-  title: 'Row Split Lines',
-  split: { visible: true, rows: 2, col:1 },
-  xAxis: [{
-    categories: [
-      'home',   'sky',
-      '태풍',   'def',
-      '지리산', 'zzz',
-      'ttt',    'taaatt',
-      '백두산', '낙동강'
-    ],
-  },{
-    linear: {
-      visible: true
-    }
-  }],
-  yAxis: [ {}, { row: 1, position: 'opposite' } ],
+  type: "line",
+  options: {
+    theme: "real",
+    palette: "unicorn",
+  },
+  title: {
+    text: "xAxis",
+    style: {
+      fontSize: "48px",
+      fontWeight: "bold",
+    },
+  },
+  split: { visible: true, rows: 4, col: 1 },
+  xAxis: [
+    {
+      type: "category",
+      categories: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+      tick: { visible: true },
+      row: 0,
+    },
+    {
+      type: "linear",
+      tick: { visible: true },
+      row: 1,
+    },
+    {
+      type: "time",
+      tick: { visible: true },
+      row: 2,
+    },
+    {
+      type: "log",
+      tick: {
+        visible: true,
+        stepInterval: 0.1,
+      },
+      row: 3,
+    },
+  ],
+  yAxis: [
+    { tick: { visible: false } },
+    { row: 1, tick: { visible: false }, position: "opposite" },
+    { row: 2, tick: { visible: false } },
+    {
+      row: 3,
+      tick: { visible: false },
+      position: "opposite",
+      type: "log",
+    },
+  ],
+  tooltip: false,
+  legend: false,
+  templates: {
+    series: {
+      marker: false,
+      style: {
+        strokeWidth: "3px",
+      },
+    },
+  },
   series: [
     {
-      lineType: 'spline',
-      pointLabel: true,
-      data: [
-           7,  11, 9, 7.5,
-        15.3,  13, 7,   9,
-          11, 2.5
-      ],
+      template: "series",
+      type: "area",
+      lineType: "spline",
+      data: [7, 11, 9, 7.5, 15.3, 13, 7, 9, 11, 2.5],
       xAxis: 0,
+      yAxis: 0,
     },
     {
-      yAxis: 1,
-      lineType: 'spline',
-      pointLabel: true,
-      data: [
-           7,  10,  8, 6.5,
-        15.3,  13, 10, 9.5,
-        11.5, 3.5
-      ],
+      template: "series",
+      lineType: "spline",
+      data: [7, 10, 8, 6.5, 15.3, 13, 10, 9.5, 11.5, 3.5],
       xAxis: 1,
+      yAxis: 1,
     },
-    // {
-    //   yAxis: 2,
-    //   lineType: 'spline',
-    //   pointLabel: true,
-    //   data: [
-    //        7,  10,  8, 6.5,
-    //     15.3,  13, 10, 9.5,
-    //     11.5, 3.5
-    //   ]
-    // }
-  ]
+    {
+      template: "series",
+      lineType: "step",
+      data: [7, 10, 8, 6.5, 15.3, 13, 10, 9.5, 11.5, 3.5],
+      xStart: "1994",
+      xStep: "1y",
+      xAxis: 2,
+      yAxis: 2,
+    },
+    {
+      template: "series",
+      lineType: "spline",
+      data: [1, 2, 4, 8, 16, 32, 64, 128, 256, 512],
+      xAxis: 3,
+      yAxis: 3,
+    },
+  ],
 };
 
 let chart;
