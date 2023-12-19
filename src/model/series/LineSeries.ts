2a94d78f29a417f04665c13e10f37647d9e8c945
WooriPbg <wooripbg@gmail.com>
2023-12-19 16:44:31 +0900

    area legend label

--- src/model/series/LineSeries.ts
+++ src/model/series/LineSeries.ts
@@ -318,8 +318,8 @@ export class AreaSeries extends LineSeries {
     //-------------------------------------------------------------------------
     // constructor
     //-------------------------------------------------------------------------
-    constructor(chart: IChart) {
-        super(chart);
+    constructor(chart: IChart, name?: string) {
+        super(chart, name);
 
         this.marker.visible = chart.isPolar();
     }
