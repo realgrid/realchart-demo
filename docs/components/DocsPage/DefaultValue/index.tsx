9db65e33bf19c27a758a9842f0e2f8038f9eeb82
benny-yoo <123144980+benny-yoo@users.noreply.github.com>
2024-01-02 16:09:42 +0900

    docs default style

--- docs/components/DocsPage/DefaultValue/index.tsx
+++ docs/components/DocsPage/DefaultValue/index.tsx
@@ -2,13 +2,13 @@ import { Content } from '../Content';
 import styles from './index.module.css'
 
 export const DefaultValue = ({ value, contents }: { value: string, contents: string }) => {
-  const spcValues = ['null', 'undefined', 'true', 'false', 'NaN'];
+  const spcValues = ['null', 'true', 'false', 'NaN'];
   const color = spcValues.includes(value) ? 'var(--shiki-token-keyword)' : 
     /'.*'/.test(value) ? 'var(--shiki-token-string-expression)' : 'var(--shiki-color-text)';
 
   return (
     <p className={styles.default}>
-      default: <span style={{ color, fontWeight: 700 }}>{value}</span> {contents}<br/>
+      default: <span style={{ color }}>{value}</span> {contents}<br/>
     </p>
   );
 };
