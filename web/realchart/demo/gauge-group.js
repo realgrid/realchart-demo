19d1756fca5975b014631a4161e8172d53d97978
benny-yoo <123144980+benny-yoo@users.noreply.github.com>
2024-01-04 13:48:15 +0900

    Update gauge-group.js

--- web/realchart/demo/gauge-group.js
+++ web/realchart/demo/gauge-group.js
@@ -1,147 +1,152 @@
 /**
  * @demo
- * 
+ *
  */
 const config = {
-    templates: {
-        gauge: {
-            label: {
-                numberFormat: '#00.#',
-            }
-        }
+  templates: {
+    gauge: {
+      label: {
+        numberFormat: '#00.#',
+      },
     },
-    options: {
-        // animatable: false,
-        // theme: 'dark',
-        credits: {
-            // visible: false,
-            // verticalAlign: 'top'
-            // align: 'center'
-        }
-    },
-    title: "Circle Gauge Group",
-    gauge: [{
-        children: [{
-            name: 'gauge1',
-            template: 'gauge',
-            value: Math.random() * 100,
-            valueRim: {
-                style: {
-                    fill: '#00aaff'
-                }
+  },
+  options: {
+    palette: 'unicorn',
+  },
+  title: 'Circle Gauge Group',
+  gauge: [
+    {
+      colorByPoints: true,
+      children: [
+        {
+          name: 'gauge1',
+          template: 'gauge',
+          value: Math.random() * 100,
+          valueRim: {
+            style: {
+              fill: 'var(--color-1)',
             },
-            label: {
-                style: {
-                    fill: '#00aaff'
-                },
-                text: "<t style='fill:gray'>Time -</t> ${value}"
-            }
-        }, {
-            name: 'gauge2',
-            template: 'gauge',
-            value: Math.random() * 100,
-            valueRim: {
-                style: {
-                    fill: '#ffaa00'
-                }
+          },
+          rim: {
+            style: {
+              fill: 'yellow',
             },
-            label: {
-                style: {
-                    fill: '#ffaa00'
-                },
-                text: "<t style='fill:gray'>Run -</t> ${value}"
-            }
-        }, {
-            name: 'gauge3',
-            template: 'gauge',
-            value: Math.random() * 100,
-            valueRim: {
-                style: {
-                    fill: '#88cc00'
-                }
+          },
+          label: {
+            style: {
+              fill: 'var(--color-1)',
             },
-            label: {
-                style: {
-                    fill: '#88cc00'
-                },
-                text: "<t style='fill:gray'>Walk -</t> ${value}"
-            }
-        }, {
-            name: 'gauge4',
-            template: 'gauge',
-            value: Math.random() * 100,
-            valueRim: {
-                style: {
-                    fill: 'var(--color-3)',
-                }
-            },
-            label: {
-                style: {
-                    fill: 'var(--color-3)'
-                },
-                text: "<t style='fill:gray'>Kcal -</t> ${value}"
-            }
-        }],
-        innerRadius: '50%',
-        sweepAngle: 270,
-        valueRim: {
-            ranges: [{
-                toValue: 25,
-                color: 'green'
-            }, {
-                toValue: 50,
-                color: '#0000cc'
-            }, {
-                toValue: 75,
-                color: '#ffaa00'
-            }, {
-                color: 'red'
-            }]
+            text: "<t style='fill:gray'>Time -</t> ${value}",
+          },
         },
-        panelStyle: {
-            stroke: 'lightblue',
-            borderRadius: '10px'
-        }
-    }]
-}
+        {
+          name: 'gauge2',
+          template: 'gauge',
+          value: Math.random() * 100,
+          valueRim: {
+            style: {
+              fill: 'var(--color-2)',
+            },
+          },
+          label: {
+            style: {
+              fill: 'var(--color-2)',
+            },
+            text: "<t style='fill:gray'>Run -</t> ${value}",
+          },
+        },
+        {
+          name: 'gauge3',
+          template: 'gauge',
+          value: Math.random() * 100,
+          valueRim: {
+            style: {
+              fill: 'var(--color-3)',
+            },
+          },
+          label: {
+            style: {
+              fill: 'var(--color-3)',
+            },
+            text: "<t style='fill:gray'>Walk -</t> ${value}",
+          },
+        },
+        {
+          name: 'gauge4',
+          template: 'gauge',
+          value: Math.random() * 100,
+          valueRim: {
+            style: {
+              fill: 'var(--color-4)',
+            },
+          },
+          label: {
+            style: {
+              fill: 'var(--color-4)',
+            },
+            text: "<t style='fill:gray'>Kcal -</t> ${value}",
+          },
+        },
+      ],
+      innerRadius: '30%',
+      sweepAngle: 270,
+    },
+  ],
+};
 
 let animate;
 let chart;
 let timer;
 
 function setActions(container) {
-    createCheckBox(container, 'Debug', function (e) {
-        RealChart.setDebugging(_getChecked(e));
-        chart.render();
-    }, false);
-    createButton(container, 'Test', function(e) {
-        alert('hello');
-    });
-    createCheckBox(container, 'label.animatable', function (e) {
-        config.gauge.label.animatable = _getChecked(e);
-        chart.load(config);
-    }, true);
-    createButton(container, 'Run', function(e) {
-        clearInterval(timer);
-        timer = setInterval(() => {
-            for (let i = 1; i <= 4; i++) {
-                chart.getGauge('gauge' + i).setValue(Math.random() * 100);
-            }
-        }, 2000);
-    });
-    createButton(container, 'Stop', function(e) {
-        clearInterval(timer);
-    });
-    createListBox(container, "options.theme", ['', 'dark'], function (e) {
-        config.options.theme = _getValue(e);
-        chart.load(config, animate);
-    }, 'dark');
+  createCheckBox(
+    container,
+    'Debug',
+    function (e) {
+      RealChart.setDebugging(_getChecked(e));
+      chart.render();
+    },
+    false
+  );
+  createButton(container, 'Test', function (e) {
+    alert('hello');
+  });
+  createCheckBox(
+    container,
+    'label.animatable',
+    function (e) {
+      config.gauge.label.animatable = _getChecked(e);
+      chart.load(config);
+    },
+    true
+  );
+  createButton(container, 'Run', function (e) {
+    clearInterval(timer);
+    timer = setInterval(() => {
+      for (let i = 1; i <= 4; i++) {
+        chart.getGauge('gauge' + i).setValue(Math.random() * 100);
+      }
+    }, 2000);
+  });
+  createButton(container, 'Stop', function (e) {
+    clearInterval(timer);
+  });
+  createListBox(
+    container,
+    'options.theme',
+    ['', 'dark'],
+    function (e) {
+      config.options.theme = _getValue(e);
+      chart.load(config, animate);
+    },
+    'dark'
+  );
 }
 
 function init() {
-    console.log(RealChart.getVersion());
-    // RealChart.setDebugging(true);
+  console.log(RealChart.getVersion());
+  // RealChart.setDebugging(true);
 
-    chart = RealChart.createChart(document, 'realchart', config);
-    setActions('actions')
+  chart = RealChart.createChart(document, 'realchart', config);
+  setActions('actions');
 }
