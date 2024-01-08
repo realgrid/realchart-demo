485d08248c0be65210b38d48d9fb4de91a0776f7
dltlghkd930217 <58334987+dltlghkd930217@users.noreply.github.com>
2024-01-08 12:55:35 +0900

    fiddle: forum

---
+++ web/realchart/fiddle/forum/forum-21607.js
@@ -0,0 +1,38 @@
+const config = {
+  title: "Point Style Callback",
+  options: {
+      // animatable: false
+  },
+  xAxis: {
+      categories: ['쓰리엠', '아디다스', '디즈니', '이마트', '메리어트', '시세이도'],
+  },
+  yAxis: {
+      title: "Vertical 수직축 Axis",
+  },
+  series: [{
+      name: 'column1',
+      pointLabel: {
+          visible: true,
+          position: 'inside',
+          effect: 'outline',
+      },
+      pointStyleCallback: args => {
+          if (args.index == 0) {
+              return { fill: 'lightgray' }
+          }
+          else if(args.yValue === args.yMax) {
+              return { fill: 'green' }
+          }
+      },
+      data: [11, 22, 15, 9, 19, 13, 27, 15]
+  }]
+}
+let animate = false;
+let chart;
+
+function init() {
+  chart = RealChart.createChart(document, "realchart", config);
+  setTimeout(() => {
+    chart.series.setValueAt(0, 20)
+  }, 2000);
+}
