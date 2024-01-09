0facc5b11886c13f4b13170858a34b427f7d9b47
dltlghkd930217 <dltlghkd930217@gmail.com>
2024-01-09 10:33:07 +0000

    Copied folder for fiddle demo

---
+++ tutorial/annotations/demo.js
@@ -0,0 +1,94 @@
+const config = {
+  assets: [{
+    id: 'brown-mint',
+    type: 'colors',
+    colors: ['#009D92', '#47AFA8', '#835337', '#593219', '#937B6F']
+  }],
+  title: {
+    align: 'left',
+    text: '2023년 11월',
+    gap: 10,
+    style: {
+      fontSize: '16px',
+      padding: '2px 5px'
+    }
+  },
+  subtitle: {
+    align: 'left',
+    text: '여론 조사',
+    style: {
+      fill: 'black',
+      fontSize: '32px',
+      fontWeight: 'bold',
+      marginBottom: '10px'
+    }
+  },
+  annotations: [{
+    text: `<t>긍정적으로 평가한다</t><br><b style="font-size:20pt">32.5</b>`,
+    offsetX: 160,
+    offsetY: 50,
+    align: 'center',
+    style: {
+      fill: '#009D92',
+      textAlign: 'center'
+    }
+  }, {
+    text: `<t>부정적으로 평가한다</t><br><b style="font-size:30pt">60.5</b>`,
+    offsetX: -250,
+    verticalAlign: 'bottom',
+    align: 'center',
+    style: {
+      fill: '#835337',
+      textAlign: 'center'
+    }
+  }, {
+    imageUrl: '../assets/images/seoul.png',
+    front: true,
+    width: 260,
+    align: 'center',
+    verticalAlign: 'middle',
+    offsetX: -10,
+    offsetY: -25
+  }],
+  series: {
+    type: 'pie',
+    radius: '50%',
+    innerRadius: '50%',
+    tooltipText: '${x}: ${y}%',
+    data: [{
+      label: '매우 긍정',
+      value: 11.1
+    }, {
+      label: '어느 정도 긍정',
+      value: 21.4
+    }, {
+      label: '어느 정도 부정',
+      value: 16.9
+    }, {
+      label: '매우 부정',
+      value: 43.6
+    }, {
+      label: '모름|무응답',
+      value: 7.1
+    }],
+    pointLabel: {
+      visible: true,
+      numberFormat: '#.00',
+      suffix: '%',
+      position: 'inside',
+      style: {
+        fill: '#fff'
+      },
+      text: '${x}<br>${y}'
+    },
+    pointColors: 'brown-mint'
+  }
+};
+let animate = false;
+let chart;
+function init() {
+  console.log('RealChart v' + RealChart.getVersion());
+  // RealChart.setDebugging(true);
+  RealChart.setLogging(true);
+  chart = RealChart.createChart(document, 'realchart', config);
+}
