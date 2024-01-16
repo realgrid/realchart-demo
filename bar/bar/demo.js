a3b7f0ae4addb35e4183ff532c0b6d9700831818
dltlghkd930217 <dltlghkd930217@gmail.com>
2024-01-16 05:20:41 +0000

    Copied folder for fiddle demo

--- bar/bar/demo.js
+++ bar/bar/demo.js
@@ -6,16 +6,19 @@
 const config = {
   title: '연도별 서울시 평균 대기질 지수',
   options: {},
+  inverted: false,
   xAxis: {
     title: '서울시',
     categories: ['2014', '2015', '2016', '2017', '2018', '2019', '2020'],
     grid: {
       visible: true
     },
-    label: {}
+    label: {},
+    reversed: false
   },
   yAxis: {
-    title: '대기질 지수<br><t style="fill:gray;font-size:0.9em;">(Air Quality Index, AQI)</t>'
+    title: '대기질 지수<br><t style="fill:gray;font-size:0.9em;">(Air Quality Index, AQI)</t>',
+    reversed: false
   },
   series: [{
     name: '대기질',
