d4e3c70da57eedac98f9f12cc2f1762c649b38e6
dltlghkd930217 <dltlghkd930217@gmail.com>
2024-01-25 05:53:12 +0000

    Copied folder for fiddle demo

--- tutorial/rcchartdata/demo.js
+++ tutorial/rcchartdata/demo.js
@@ -402,8 +402,8 @@ function updateValue() {
   const high = ds.getValue(row, 'highprc');
   const low = ds.getValue(row, 'lowprc');
   const close = Math.floor(Math.random() * (high - low + 1) + low);
-  ds.setValue(row, 'close', close);
-  if (close == ds.getValue(row, 'close')) {
+  ds.setValue(row, 'closeprc', close);
+  if (close == ds.getValue(row, 'closeprc')) {
     console.log('updated', close);
   }
 }
