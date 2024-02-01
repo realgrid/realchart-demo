84de390d8922248222e78ecbeffd333af80a6d46
dltlghkd930217 <58334987+dltlghkd930217@users.noreply.github.com>
2024-02-01 16:37:22 +0900

    update docs lic

--- docs/public/realchart/realchart-lic.js
+++ docs/public/realchart/realchart-lic.js
@@ -1,3 +1,2 @@
-// 2024-06-30	localhost,127.0.0.1,-.realchart.co.kr,192.168.1.57
-var realChartLic =
-    'upVcPE+wPOkOR/egW8JuxkM/nBOseBrflwxYpzGZyYm9cY8amGDkiMnVeQKUHJDjW2y71jtk+ws+gonfDRQYjrpym6cOBWhL7QvIe6ncLxxkCy3qB6dz6zU2pJNMkfVHkj3EVL0qXejRB4hu3LIgQZMVWpaY093J';
+// 2024-06-30	localhost,127.0.0.1,*.realchart.co.kr,192.168.1.57,192.168.1.17
+var realChartLic = 'upVcPE+wPOkOR/egW8JuxkM/nBOseBrflwxYpzGZyYm9cY8amGDkiMnVeQKUHJDjW2y71jtk+ws+gonfDRQYjrpym6cOBWhL7QvIe6ncLxxBiSwX8pMxj1E6+suzkx8766QL5aIcO/a1ymF8pLYy6+l3iLgJBejudAeQQV4lYgQ=';
