34845113d8183b3c57a1d0846d4422f62a5670a3
dltlghkd930217 <58334987+dltlghkd930217@users.noreply.github.com>
2024-02-01 11:31:30 +0900

    vector series e2e

--- test/e2e/PWTester.ts
+++ test/e2e/PWTester.ts
@@ -1,113 +1,144 @@
-import { Page, ElementHandle, TestInfo, expect } from '@playwright/test';
-import { IPoint } from '../../src/common/Point';
-import { IRect } from '../../src/common/Rectangle';
-import { Chart } from '../../src/model/Chart';
-import path from 'path';
+import { Page, ElementHandle, TestInfo, expect } from "@playwright/test";
+import { IPoint } from "../../src/common/Point";
+import { IRect } from "../../src/common/Rectangle";
+import { Chart } from "../../src/model/Chart";
+import path from "path";
 export class PWTester {
+  //-------------------------------------------------------------------------
+  // consts
+  //-------------------------------------------------------------------------
+  static readonly VIEWPORT_SIZE = {
+    width: 1920,
+    height: 1080,
+  };
 
-	//-------------------------------------------------------------------------
-	// consts
-	//-------------------------------------------------------------------------
-	static readonly VIEWPORT_SIZE = {
-		width: 1920,
-		height: 1080,
-	};
+  //-------------------------------------------------------------------------
+  // static members
+  //-------------------------------------------------------------------------
+  static same(v1: number, v2: number, round = true): boolean {
+    return round ? Math.round(v1) === Math.round(v2) : v1 === v2;
+  }
 
-	//-------------------------------------------------------------------------
-	// static members
-	//-------------------------------------------------------------------------
-	static same(v1: number, v2: number, round = true): boolean {
-		return round ? Math.round(v1) === Math.round(v2) : v1 === v2;
-	}
+  static async goto(page: Page, url: string): Promise<void> {
+    page.on("domcontentloaded", (page) => {});
+    await page.setViewportSize(this.VIEWPORT_SIZE);
+    await page
+      .goto("http://localhost:6010/realchart/" + url)
+      .catch((err) => ">> " + console.error(err));
+  }
 
-	static async goto(page: Page, url: string): Promise<void> {
-		page.on('domcontentloaded', (page) => {});
-		await page.setViewportSize(this.VIEWPORT_SIZE);
-		await page
-			.goto('http://localhost:6010/realchart/' + url)
-			.catch((err) => '>> ' + console.error(err));
-	}
+  static async getBounds(elt: ElementHandle): Promise<IRect> {
+    return await elt.evaluate((elt) => {
+      const { x, y, width, height } = (elt as Element).getBoundingClientRect();
+      return { x, y, width, height };
+    });
+  }
 
-	static async getBounds(elt: ElementHandle): Promise<IRect> {
-		return await elt.evaluate((elt) => {
-			const { x, y, width, height } = (
-				elt as Element
-			).getBoundingClientRect();
-			return { x, y, width, height };
-		});
-	}
+  static async getAxis(page: Page, xy: "x" | "y"): Promise<ElementHandle> {
+    return await page.$(".rct-axis" + `[xy=${xy}]`);
+  }
 
-	static async getAxis(page: Page, xy: 'x' | 'y'): Promise<ElementHandle> {
-		return await page.$('.rct-axis' + `[xy=${xy}]`);
-	}
+  static async getAxisLine(page: Page, xy: "x" | "y"): Promise<ElementHandle> {
+    const axis = await this.getAxis(page, xy);
+    return await axis.$(".rct-axis-line");
+  }
 
-	static async getAxisLine(
-		page: Page,
-		xy: 'x' | 'y'
-	): Promise<ElementHandle> {
-		const axis = await this.getAxis(page, xy);
-		return await axis.$('.rct-axis-line');
-	}
+  static async getGridBounds(page: Page): Promise<IRect> {
+    const grids = await page.$(".rct-axis-grids");
+    return await this.getBounds(grids);
+  }
 
-	static async getGridBounds(page: Page): Promise<IRect> {
-		const grids = await page.$('.rct-axis-grids');
-		return await this.getBounds(grids);
-	}
+  static async getTranslate(elt: ElementHandle): Promise<IPoint> {
+    const bv = await elt.evaluate((elt) => {
+      const svgElement = elt as SVGSVGElement;
+      const transformList = svgElement.transform.baseVal;
+      if (transformList.numberOfItems > 0) {
+        // 첫 번째 변환을 가져오기
+        const firstTransform = transformList.getItem(0);
+        return {
+          x: firstTransform.matrix.e,
+          y: firstTransform.matrix.f,
+        };
+      }
+      return { x: 0, y: 0 }; // 변환이 없는 경우 기본값 반환
+    });
+    return bv;
+  }
 
-	static async getTranslate(elt: ElementHandle): Promise<IPoint> {
-		const bv = await elt.evaluate((elt) => {
-			const svgElement = elt as SVGSVGElement;
-			const transformList = svgElement.transform.baseVal;
-			if (transformList.numberOfItems > 0) {
-				// 첫 번째 변환을 가져오기
-				const firstTransform = transformList.getItem(0);
-				return {
-					x: firstTransform.matrix.e,
-					y: firstTransform.matrix.f,
-				};
-			}
-			return { x: 0, y: 0 }; // 변환이 없는 경우 기본값 반환
-		});
-		return bv;
-	}
+  static async getPathDValue(elt: ElementHandle): Promise<string | null> {
+    const dValue = await elt.evaluate((elt) => {
+      const pathElement = elt as SVGPathElement;
+      return pathElement.getAttribute("d");
+    });
+    return dValue;
+  }
 
-	static async getPathDValue(elt: ElementHandle): Promise<string | null> {
-		const dValue = await elt.evaluate((elt) => {
-			const pathElement = elt as SVGPathElement;
-			return pathElement.getAttribute('d');
-		});
-		return dValue;
-	}
+  static async getConfig(page: any): Promise<Chart> {
+    const config = await page.evaluate("config");
+    const chart = new Chart(config);
 
-    static async getConfig(page: any): Promise<Chart> {
-        const config = await page.evaluate('config');
-        const chart = new Chart(config);
-        
-        chart.prepareRender();
-        return chart;
+    chart.prepareRender();
+    return chart;
+  }
+
+  static async getModel(page: any): Promise<any> {
+    return await page.evaluate("chart.$_p.model");
+  }
+
+  static async sleep(time: number = 500): Promise<void> {
+    await new Promise((resolve) => setTimeout(resolve, time));
+  }
+
+  static async testChartBySnapshot(
+    page: Page,
+    testInfo: TestInfo,
+    description?: string
+  ) {
+    const chartElementId = "#realchart";
+    // const folderPath = testInfo.snapshotDir.split("/").slice(0, -1).join("/");
+    // const snapshotFolder = path.join(folderPath, 'snapshot');
+    // console.log(path.join(snapshotFolder, 'test.png'))
+    // const screenShot = await page.locator(chartElementId).screenshot({path: path.join(snapshotFolder, 'test.png')});
+    const screenShot = await page.locator(chartElementId).screenshot();
+
+    await expect(screenShot).toMatchSnapshot({ maxDiffPixels: 10 });
+    await testInfo.attach("screenshot", {
+      body: screenShot,
+      contentType: "image/png",
+    });
+    if (!description) {
+      return;
     }
+    await testInfo.attach("스냅샷 설명", {
+      body: description,
+      contentType: "text/plain",
+    });
+  }
 
-    static async getModel(page: any): Promise<any> {
-        return await page.evaluate('chart.$_p.model');
+  static async mouseHover(page: Page, elt: ElementHandle) {
+    await page.mouse.move(0, 0);
+    const boundingBox = await elt.boundingBox();
+
+    if (boundingBox) {
+      await page.mouse.move(
+        boundingBox.x + boundingBox.width / 2,
+        boundingBox.y + boundingBox.height / 2
+      );
+    } else {
+      console.error("DOM 요소의 위치를 가져올 수 없습니다.");
     }
+  }
+  static async mouseClick(page: Page, elt: ElementHandle) {
+    await page.mouse.move(0, 0);
+    const boundingBox = await elt.boundingBox();
 
-	static async sleep(time: number = 500): Promise<void> {
-		await new Promise((resolve) => setTimeout(resolve, time));
-	}
-
-	static async testChartBySnapshot(page: Page, testInfo: TestInfo, description?: string) {
-		const chartElementId = '#realchart';
-		// const folderPath = testInfo.snapshotDir.split("/").slice(0, -1).join("/");
-		// const snapshotFolder = path.join(folderPath, 'snapshot');
-		// console.log(path.join(snapshotFolder, 'test.png'))
-		// const screenShot = await page.locator(chartElementId).screenshot({path: path.join(snapshotFolder, 'test.png')});
-		const screenShot = await page.locator(chartElementId).screenshot();
-
-		await expect(screenShot).toMatchSnapshot({ maxDiffPixels: 10 });
-		await testInfo.attach('screenshot', { body: screenShot, contentType: 'image/png' });
-		if (!description) {
-			return;
-		}
-		await testInfo.attach('스냅샷 설명', { body: description, contentType: 'text/plain' });
-	}
+    if (boundingBox) {
+      await page.mouse.click(
+        boundingBox.x + boundingBox.width / 2,
+        boundingBox.y + boundingBox.height / 2
+      );
+    } else {
+      console.error("DOM 요소의 위치를 가져올 수 없습니다.");
+    }
+  }
 }
---
+++ test/e2e/boundary/series/vector.L.spec.ts
@@ -0,0 +1,454 @@
+import { ArrowHead } from './../../../../src/model/series/VectorSeries';
+////////////////////////////////////////////////////////////////////////////////
+// vector.L.spec.ts
+// 2024. 01. 31. created by dltlghkd930217
+// -----------------------------------------------------------------------------
+// Copyright (c) 2023 Wooritech Inc.
+// All rights reserved.
+////////////////////////////////////////////////////////////////////////////////
+
+import { test } from '@playwright/test';
+import { expect } from 'chai';
+import { PWTester } from '../../PWTester';
+declare global {
+    var eventCnt: number;
+}
+
+/** @TODO: 코드테스트 추가, col, noClip, row, */
+test.describe('series, vector test', () => {
+    const url = 'boundary/empty.html?debug';
+
+    const createVectorData = (type) => {
+        const data: Array<any> = [];
+        let x, y, length, angle;
+
+        for (x = 5; x < 100; x += 5) {
+            for (y = 5; y < 100; y += 5) {
+                length = Math.round(200 - (x + y));
+                angle = Math.round(((x + y) / 200) * 360);
+                switch (type) {
+                    case 'y':
+                        data.push(y);
+                        break;
+                    case 'noX':
+                        data.push([y, length, angle]);
+                        break;
+                    case 'all':
+                        data.push([x, y, length, angle]);
+                        break;
+                    case 'json':
+                        data.push({
+                            x,
+                            y,
+                            length,
+                            angle
+                        });
+                        break;
+                    case 'json2':
+                        data.push({
+                            a: x,
+                            b: y,
+                            c: length,
+                            d: angle
+                        });
+                        break;
+                }
+                // data.push([x, y, length, angle].join(", "));
+            }
+        }
+
+        return data;
+    };
+
+    let chart;
+
+    let config: any;
+
+    test.beforeEach(async ({ page }) => {
+        config = {
+            title: 'Boundary',
+            series: {
+                type: 'vector',
+                data: undefined
+            }
+        };
+        await PWTester.goto(page, url);
+    });
+
+    test('init', async ({ page }, testInfo) => {
+        const container = await page.$('#realchart');
+        expect(container).exist;
+        await PWTester.testChartBySnapshot(page, testInfo);
+    });
+
+    test('dataform:y', async ({ page }, testInfo) => {
+        const container = await page.$('#realchart');
+        expect(container).exist;
+
+        config.series.data = createVectorData('y');
+
+        await page.evaluate((newConfig) => {
+            chart.load(newConfig, false).render();
+        }, config);
+
+        await PWTester.testChartBySnapshot(page, testInfo);
+    });
+
+    test('dataform:noX', async ({ page }, testInfo) => {
+        const container = await page.$('#realchart');
+        expect(container).exist;
+
+        config.series.data = createVectorData('noX');
+
+        await page.evaluate((newConfig) => {
+            chart.load(newConfig, false).render();
+        }, config);
+
+        await PWTester.testChartBySnapshot(page, testInfo);
+    });
+
+    test('dataform:all', async ({ page }, testInfo) => {
+        const container = await page.$('#realchart');
+        expect(container).exist;
+
+        config.series.data = createVectorData('all');
+
+        await page.evaluate((newConfig) => {
+            chart.load(newConfig, false).render();
+        }, config);
+
+        await PWTester.testChartBySnapshot(page, testInfo);
+    });
+
+    test('dataform:json', async ({ page }, testInfo) => {
+        const container = await page.$('#realchart');
+        expect(container).exist;
+
+        config.series.data = createVectorData('json');
+
+        await page.evaluate((newConfig) => {
+            chart.load(newConfig, false).render();
+        }, config);
+
+        await PWTester.testChartBySnapshot(page, testInfo);
+    });
+
+    test('xField, yField, lengthField, angleField', async ({ page }, testInfo) => {
+        const container = await page.$('#realchart');
+        expect(container).exist;
+
+        config.series.data = createVectorData('json2');
+        config.series.xField = 'a';
+        config.series.yField = 'b';
+        config.series.lengthField = 'c';
+        config.series.angleField = 'd';
+
+        await page.evaluate((newConfig) => {
+            chart.load(newConfig, false).render();
+        }, config);
+
+        await PWTester.testChartBySnapshot(page, testInfo);
+    });
+    const arrowHeads = ['close', 'none', 'open'];
+
+    for (let arrowHead of arrowHeads) {
+        test('arrowHead: ' + arrowHead, async ({ page }, testInfo) => {
+            const container = await page.$('#realchart');
+            expect(container).exist;
+
+            config.series.data = createVectorData('json');
+            config.series.arrowHead = arrowHead;
+            await page.evaluate((newConfig) => {
+                chart.load(newConfig, false).render();
+            }, config);
+
+            await PWTester.testChartBySnapshot(page, testInfo);
+        });
+    }
+
+    test('color', async ({ page }, testInfo) => {
+        const container = await page.$('#realchart');
+        expect(container).exist;
+
+        config.series.data = createVectorData('json');
+        config.series.color = 'red';
+        await page.evaluate((newConfig) => {
+            chart.load(newConfig, false).render();
+        }, config);
+
+        await PWTester.testChartBySnapshot(page, testInfo);
+    });
+
+    test('colorField', async ({ page }, testInfo) => {
+        const container = await page.$('#realchart');
+        expect(container).exist;
+
+        config.series.data = createVectorData('json').map((e, i) => {
+            return { ...e, color: i % 2 === 0 ? 'red' : 'green' };
+        });
+
+        await page.evaluate((newConfig) => {
+            chart.load(newConfig, false).render();
+        }, config);
+
+        await PWTester.testChartBySnapshot(page, testInfo);
+    });
+
+    test('hoverStyle', async ({ page }, testInfo) => {
+        const container = await page.$('#realchart');
+        expect(container).exist;
+
+        config.series.data = createVectorData('json');
+        config.series.hoverStyle = {
+            stroke: 'red'
+        };
+
+        await page.evaluate((newConfig) => {
+            chart.load(newConfig, false).render();
+        }, config);
+
+        await page.mouse.move(114, 479);
+        await PWTester.sleep(300);
+        await page.mouse.click(114, 479);
+        await PWTester.sleep(300);
+
+        await page.evaluate(() => {
+            const elt = document.getElementsByClassName('rct-tooltip')[0] as HTMLElement;
+            elt.style.visibility = 'visible';
+        });
+
+        await PWTester.testChartBySnapshot(page, testInfo);
+    });
+
+    test('label', async ({ page }, testInfo) => {
+        const container = await page.$('#realchart');
+        expect(container).exist;
+        config.legend = true;
+
+        config.series.data = createVectorData('json');
+        config.series.label = 'new Label!';
+
+        await page.evaluate((newConfig) => {
+            chart.load(newConfig, false).render();
+        }, config);
+
+        await PWTester.testChartBySnapshot(page, testInfo);
+    });
+
+    test('maxLength', async ({ page }, testInfo) => {
+        const container = await page.$('#realchart');
+        expect(container).exist;
+        config.series.maxLength = 10;
+        config.series.data = createVectorData('json');
+
+        await page.evaluate((newConfig) => {
+            chart.load(newConfig, false).render();
+        }, config);
+
+        await PWTester.testChartBySnapshot(page, testInfo);
+    });
+
+    test('name', async ({ page }, testInfo) => {
+        const container = await page.$('#realchart');
+        expect(container).exist;
+        config.legend = true;
+
+        config.series.name = 'series name!';
+        config.series.data = createVectorData('json');
+
+        await page.evaluate((newConfig) => {
+            chart.load(newConfig, false).render();
+        }, config);
+
+        await PWTester.testChartBySnapshot(page, testInfo);
+    });
+
+    test('onPointClick', async ({ page }, testInfo) => {
+        const container = await page.$('#realchart');
+        expect(container).exist;
+        config.series.data = createVectorData('json');
+
+        await page.evaluate((newConfig) => {
+            window.eventCnt = 0;
+            newConfig.series.onPointClick = (args) => {
+                window.eventCnt++;
+            };
+            chart.load(newConfig, false).render();
+        }, config);
+
+        const element = (await page.$$('.rct-point'))[0];
+        await PWTester.mouseClick(page, element);
+        await PWTester.mouseClick(page, element);
+        await PWTester.mouseClick(page, element);
+
+        const count = await page.evaluate(() => {
+            return window.eventCnt;
+        });
+
+        expect(count).is.equal(3);
+    });
+
+    test('onPointHover', async ({ page }, testInfo) => {
+        const container = await page.$('#realchart');
+        expect(container).exist;
+        config.series.data = createVectorData('json');
+
+        await page.evaluate((newConfig) => {
+            window.eventCnt = 0;
+            newConfig.series.onPointHover = (args) => {
+                window.eventCnt++;
+            };
+            chart.load(newConfig, false).render();
+        }, config);
+
+        const element = await page.$$('.rct-point');
+        await PWTester.mouseClick(page, element[0]);
+        await page.mouse.click(0, 0);
+        await PWTester.mouseClick(page, element[1]);
+        await page.mouse.click(0, 0);
+        await PWTester.mouseClick(page, element[2]);
+        await page.mouse.click(0, 0);
+
+        const count = await page.evaluate(() => {
+            return window.eventCnt;
+        });
+
+        expect(count).is.equal(3);
+    });
+
+    test('pointColors', async ({ page }, testInfo) => {
+        const container = await page.$('#realchart');
+        expect(container).exist;
+
+        config.series.data = createVectorData('json');
+        config.series.pointColors = ['black'];
+
+        await page.evaluate((newConfig) => {
+            chart.load(newConfig, false).render();
+        }, config);
+
+        await PWTester.testChartBySnapshot(page, testInfo);
+    });
+
+    test('pointStyle', async ({ page }, testInfo) => {
+        const container = await page.$('#realchart');
+        expect(container).exist;
+
+        config.series.data = createVectorData('json');
+        config.series.pointStyle = {
+            stroke: 'green'
+        };
+
+        await page.evaluate((newConfig) => {
+            chart.load(newConfig, false).render();
+        }, config);
+
+        await PWTester.testChartBySnapshot(page, testInfo);
+    });
+
+    test('pointStyleCallback', async ({ page }, testInfo) => {
+        const container = await page.$('#realchart');
+        expect(container).to.exist;
+
+        config.series.data = createVectorData('json');
+
+        await page.evaluate((newConfig) => {
+            newConfig.series.pointStyleCallback = (args) => {
+                if (args.angle > 180) {
+                    return {
+                        stroke: 'green'
+                    };
+                }
+            };
+            chart.load(newConfig, false).render();
+        }, config);
+
+        await PWTester.testChartBySnapshot(page, testInfo);
+    });
+
+    test('startAngle', async ({ page }, testInfo) => {
+        const container = await page.$('#realchart');
+        expect(container).exist;
+
+        config.series.data = createVectorData('json');
+        config.series.startAngle = 180;
+
+        await page.evaluate((newConfig) => {
+            chart.load(newConfig, false).render();
+        }, config);
+
+        await PWTester.testChartBySnapshot(page, testInfo);
+    });
+
+    test('tooltipText', async ({ page }, testInfo) => {
+        const container = await page.$('#realchart');
+        expect(container).exist;
+
+        config.series.data = createVectorData('json');
+
+        await page.evaluate((newConfig) => {
+            newConfig.series.tooltipText = `%{x} %{y} %{length} %{angle}`;
+
+            chart.load(newConfig, false).render();
+        }, config);
+
+        await page.mouse.move(114, 479);
+        await PWTester.sleep(300);
+        await page.mouse.click(114, 479);
+        await PWTester.sleep(300);
+
+        await page.evaluate(() => {
+            const elt = document.getElementsByClassName('rct-tooltip')[0] as HTMLElement;
+            elt.style.visibility = 'visible';
+        });
+
+        await PWTester.testChartBySnapshot(page, testInfo);
+    });
+
+    test('viewRanges, viewRangeValue', async ({ page }, testInfo) => {
+        const container = await page.$('#realchart');
+        expect(container).exist;
+
+        config.series.data = createVectorData('json');
+        config.series.viewRangeValue = 'y';
+        config.series.viewRanges = [
+            { fromValue: 0, toValue: 50, color: 'blue' },
+            { fromValue: 50, toValue: 100, color: 'red' }
+        ];
+
+        await page.evaluate((newConfig) => {
+            chart.load(newConfig, false).render();
+        }, config);
+
+        await PWTester.testChartBySnapshot(page, testInfo);
+    });
+
+    test('xStart', async ({ page }, testInfo) => {
+        const container = await page.$('#realchart');
+        expect(container).exist;
+
+        config.series.data = createVectorData('noX');
+        config.series.xStart = 40;
+
+        await page.evaluate((newConfig) => {
+            chart.load(newConfig, false).render();
+        }, config);
+
+        await PWTester.testChartBySnapshot(page, testInfo);
+    });
+
+    test('xStep', async ({ page }, testInfo) => {
+        const container = await page.$('#realchart');
+        expect(container).exist;
+
+        config.series.data = createVectorData('noX');
+        config.series.xStart = 40;
+        config.series.xStep = 2;
+
+        await page.evaluate((newConfig) => {
+            chart.load(newConfig, false).render();
+        }, config);
+
+        await PWTester.testChartBySnapshot(page, testInfo);
+    });
+});
