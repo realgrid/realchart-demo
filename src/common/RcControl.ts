fc4a70124884e53cf0b253b34e313c266da72c40
dltlghkd930217 <58334987+dltlghkd930217@users.noreply.github.com>
2024-02-15 17:36:41 +0900

    release v1.0.1

--- src/common/RcControl.ts
+++ src/common/RcControl.ts
@@ -456,7 +456,7 @@ export abstract class RcControl extends RcObject {//} RcWrappableObject {
 
         const desc = doc.createElement('desc');
         // desc.textContent = 'Created by RealChart v$Version'; // sourcemap, rollup issue
-        desc.textContent = 'Created by RealChart v1.0.0';
+        desc.textContent = 'Created by RealChart v1.0.1';
         svg.appendChild(desc);
 
         const defs = this._defs = doc.createElementNS(SVGNS, 'defs');
