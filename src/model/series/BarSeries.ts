321c488fa710b077c84ed229359f6b662e4b5a92
gslim65 <gslim@wooritech.com>
2024-02-15 16:25:02 +0900

    baseValue

--- src/model/series/BarSeries.ts
+++ src/model/series/BarSeries.ts
@@ -6,7 +6,7 @@
 // All rights reserved.
 ////////////////////////////////////////////////////////////////////////////////
 
-import { pickNum } from "../../common/Common";
+import { pickNum, pickNum3 } from "../../common/Common";
 import { RcElement } from "../../common/RcControl";
 import { RectElement } from "../../common/impl/RectElement";
 import { IAxis } from "../Axis";
@@ -135,7 +135,7 @@ export abstract class BarSeriesGroupBase<T extends BarSeriesBase> extends Cluste
     /**
      * @config
      */
-    baseValue = 0;
+    baseValue: number;
 
     //-------------------------------------------------------------------------
     // overriden members
@@ -145,7 +145,7 @@ export abstract class BarSeriesGroupBase<T extends BarSeriesBase> extends Cluste
     }
 
     override getBaseValue(axis: IAxis): number {
-        return axis._isX ? NaN : pickNum(this.baseValue, axis.getBaseValue());
+        return axis._isX ? NaN : pickNum3(this.baseValue, axis.getBaseValue(), 0);
     }
 
     protected override _doPrepareSeries(series: T[]): void {
