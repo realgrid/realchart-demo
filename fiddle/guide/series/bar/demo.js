16a2bdcd376b73245b71f5d30ee25636d3ad5531
dltlghkd930217 <58334987+dltlghkd930217@users.noreply.github.com>
2024-02-22 10:11:13 +0900

    fiddle: guide bar

---
+++ fiddle/guide/series/bar/demo.js
@@ -0,0 +1,14 @@
+const config = {
+  series: {
+    type: 'bar',
+    data: [155, 138, 122, 133, 114, 113]
+    // x축 label로 사용될 값을 지정할 수도 있다.
+    // data: [['', 155], ['', 138], ['', 122], ['', 133], ['', 114], ['', 113]],
+  }
+};
+
+let animate = false;
+let chart;
+function init() {
+  chart = RealChart.createChart(document, 'realchart', config);
+}
---
+++ web/realchart/fiddle/guide/series/bar-group.js
@@ -0,0 +1,27 @@
+const config = {
+    series: [
+        {
+            children: [
+                {
+                    name: "group1",
+                    data: [11, 22, 15, 9, 13, 27]
+                },
+                {
+                    name: "group2",
+                    data: [15, 19, 19, 6, 21, 21]
+                },
+                {
+                    name: "group3",
+                    data: [13, 17, 15, 11, 23, 17]
+                }
+            ]
+        }
+    ]
+};
+
+let animate = false;
+let chart;
+
+function init() {
+    chart = RealChart.createChart(document, 'realchart', config);
+}
