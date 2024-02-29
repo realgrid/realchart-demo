40a1a5498a5172cc9a3d21b5ac3288819f353a45
dltlghkd930217 <58334987+dltlghkd930217@users.noreply.github.com>
2024-02-29 11:05:17 +0900

    update fiddle

---
+++ fiddle/guide/series/pie-donut/demo.js
@@ -0,0 +1,31 @@
+const config = {
+  series: {
+    type: 'pie',
+    radius: '40%',
+    innerRadius: '50%',
+    legendByPoint: true,
+    pointLabel: {
+      text: '${x}<br>${y}%',
+      visible: true
+    },
+    data: [{
+      x: 'Android',
+      y: 53.51,
+      sliced: true
+    }, {
+      x: 'iOS',
+      y: 29.14
+    }, {
+      x: 'Windows',
+      y: 10.72
+    }, {
+      x: '기타',
+      y: 6.63
+    }]
+  }
+};
+let animate = false;
+let chart;
+function init() {
+  chart = RealChart.createChart(document, 'realchart', config);
+}
---
+++ web/realchart/fiddle/guide/series/pie-angle.js
@@ -0,0 +1,30 @@
+const config = {
+    series: {
+        type: 'pie',
+        startAngle: -90,
+        totalAngle: 180,
+        innerRadius: '50%',
+        radius: '70%',
+        centerY: '80%',
+        legendByPoint: true,
+        
+        pointLabel: {
+            text: '${x}<br>${y}%',
+            position: 'outside',
+            visible: true
+        },
+        data: [
+            { x: 'Android', y: 53.51, sliced: true },
+            { x: 'iOS', y: 29.14 },
+            { x: 'Windows', y: 10.72 },
+            { x: '기타', y: 6.63 }
+        ]
+    }
+};
+
+let animate = false;
+let chart;
+
+function init() {
+    chart = RealChart.createChart(document, 'realchart', config);
+}
