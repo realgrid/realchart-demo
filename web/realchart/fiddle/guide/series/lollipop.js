ceef9174e475941908f4d931607bb936f3cffc55
dltlghkd930217 <58334987+dltlghkd930217@users.noreply.github.com>
2024-03-12 11:41:18 +0900

    update: guide fiddle

---
+++ web/realchart/fiddle/guide/series/lollipop.js
@@ -0,0 +1,31 @@
+const config = {
+    options: {},
+    title: "연도별 서울시 평균 대기질 지수",
+    xAxis: {
+        title: '서울시',
+        categories: ['`14', '`15', '`16', '`17', '`18', '`19'],
+        grid: {
+            visible: true,
+        },
+    },
+    yAxis: {
+        title: '대기질 지수<br><t style="fill:gray;font-size:0.9em;">(Air Quality Index, AQI)</t>',
+    },
+    series: {
+        type: 'lollipop',
+        pointLabel: {
+            visible: true,
+        },
+        data: [155, 138, 122, 133, 114, 113],
+    }
+}
+
+
+let chart;
+
+function init() {
+    console.log('RealChart v' + RealChart.getVersion());
+    RealChart.setLogging(false);
+
+    chart = RealChart.createChart(document, 'realchart', config);
+}
---
+++ web/realchart/fiddle/guide/series/ohlc.js
@@ -0,0 +1,32 @@
+const config = {
+    options: {},
+    title: "Ohlc Series",
+    xAxis: {
+        type: 'category',
+    },
+    yAxis: {
+        yBase: null
+    },
+    series: {
+        type: 'ohlc',
+        pointLabel: true,
+        tooltipText: 'open: ${open}<br>High: ${high}<br>Low: ${low}<br>Close: ${close}',
+        data: [
+            {low: 301, open: 348, close: 395, high: 465, color: 'green'},
+            [353, 439, 480, 580],
+            [262, 370, 317, 418],
+            [302, 326, 371, 450],
+            [336, 382, 364, 420],
+            [341, 356, 381, 430],
+        ],
+    }
+}
+
+let chart;
+
+function init() {
+    console.log('RealChart v' + RealChart.getVersion());
+    RealChart.setLogging(false);
+
+    chart = RealChart.createChart(document, 'realchart', config);
+}
