698d3a79da3933ecef2067431df46de4fbc7fa5f
dltlghkd930217 <58334987+dltlghkd930217@users.noreply.github.com>
2024-03-26 10:19:01 +0900

    #614 재현예제

---
+++ web/realchart/exam/axis-grid.js
@@ -0,0 +1,77 @@
+/**
+ * @demo
+ *
+ */
+
+
+const config = {
+  title: 'Boundary',
+  xAxis: {
+      grid: {
+          visible: false,
+          firstVisible: true,
+          lastVisible: true
+      },
+  },
+  yAxis: {
+     
+  },
+  series: {
+      name: 'column1',
+      data: [1, 2, 3, 4]
+  }
+};
+
+let animate;
+let chart;
+
+function setActions(container) {
+  createCheckBox(
+    container,
+    'Debug',
+    function (e) {
+      RealChart.setDebugging(_getChecked(e));
+      chart.render();
+    },
+    false
+  );
+  createCheckBox(
+    container,
+    'Always Animate',
+    function (e) {
+      animate = _getChecked(e);
+    },
+    false
+  );
+  createButton(container, 'Test', function (e) {
+    alert('hello');
+  });
+  createListBox(
+    container,
+    'X.startOffset',
+    ['0', '0.5'],
+    function (e) {
+      config.xAxis.startOffset = _getValue(e);
+      chart.load(config);
+    },
+    '0'
+  );
+  createCheckBox(
+    container,
+    'body.circular',
+    function (e) {
+      config.body.circular = _getChecked(e);
+      chart.load(config, animate);
+    },
+    false
+  );
+}
+
+function init() {
+  console.log('RealChart v' + RealChart.getVersion());
+  // RealChart.setDebugging(true);
+  RealChart.setLogging(true);
+
+  chart = RealChart.createChart(document, 'realchart', config);
+  setActions('actions');
+}
