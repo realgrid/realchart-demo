cca848c1186046eb444ef445b68f6853a069456d
benny-yoo <123144980+benny-yoo@users.noreply.github.com>
2024-04-26 13:32:38 +0900

    docs: default axis type of series
    시리즈 기본 축 타입 관련 설명

--- src/model/series/EqualizerSeries.ts
+++ src/model/series/EqualizerSeries.ts
@@ -23,7 +23,7 @@ export class EqualizerSeriesPoint extends DataPoint {
  * Bar를 여러 개의 segment로 나눠 표시하는 시리즈.<br/>
  * 수평 또는 수직 막대로 여러 값들을 **비교**하는 데 사용한다.
  * 막대의 길이가 y값을 나타낸다.<br/>
- * 이 시리즈를 기준으로 생성되는 x축은 [category](/config/config/xAxis/category)이다.<br/><br/>
+ * X축 타입이 설정되지 않은 경우, 이 시리즈를 기준으로 생성되는 축은 [category](/config/config/xAxis/category)이다.<br/><br/>
  * 
  * {@link data}는 아래 형식들로 전달할 수 있다.<br/>
  * 
