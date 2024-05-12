2527bd5b56f3e1335ac57722511168ad63857415
benny-yoo <123144980+benny-yoo@users.noreply.github.com>
2024-05-12 18:23:20 +0900

    Update demo.js

--- blog/gender-race/demo.js
+++ blog/gender-race/demo.js
@@ -136,22 +136,9 @@ const config = {
 
 let animate = false;
 let chart;
-function setActions(container) {
-  createCheckBox(container, 'Debug', function (e) {
-    RealChart.setDebugging(_getChecked(e));
-    chart.render();
-  }, false);
-  createCheckBox(container, 'Always Animate', function (e) {
-    animate = _getChecked(e);
-  }, false);
-  createButton(container, 'Test', function (e) {
-    alert('hello');
-  });
-}
 function init() {
   console.log('RealChart v' + RealChart.getVersion());
   // RealChart.setDebugging(true);
   RealChart.setLogging(true);
   chart = RealChart.createChart(document, 'realchart', config);
-  setActions('actions');
 }
